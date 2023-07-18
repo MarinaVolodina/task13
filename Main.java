04717b61038dd2ed2975f1a897e4782448e2c549
MarinaVolodina <marigik@yandex.ru>
2023-07-18 04:00:47 +0300

    add branch lambda

--- Main.java
+++ Main.java
@@ -8,16 +8,25 @@ public class Main {
     public static void main(String[] args) {
         List<Person> persons = new ArrayList<>();
         persons.add(new Person("Ivan", "Petrov", 7));
-        persons.add(new Person("Boris", "Kim", 47));
+        persons.add(new Person("Boris", "Petrov-Vodkin", 47));
         persons.add(new Person("Oleg", "Karasev", 20));
-        persons.add(new Person("Petr", "Verov", 33));
+        persons.add(new Person("Petr", "Ivanov-Petrov-Vodkin", 33));
         persons.add(new Person("Roman", "Karamichev", 16));
 
-        Collections.sort(persons, new PersonComparator());
+        Collections.sort(persons, (o1, o2) -> {
+            int length1 = o1.getSurname().split("-").length;
+            int length2 = o2.getSurname().split("-").length;
+            if (length1 < length2) {
+                return -1;
+            }
+            if (length1 > length2) {
+                return 1;
+            }
+            return o1.getAge() - o2.getAge();
 
-        for (Person person : persons) {
-            System.out.println(person);
-        }
+        });
+
+
         persons.forEach(System.out::println);
     }
 }
